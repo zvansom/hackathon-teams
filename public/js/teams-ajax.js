3319a024f6db86db82192a5a2fd6d30446378039
Zach VanSomeren <zvansomeren@gmail.com>
2018-08-16 20:11:02 -0700

    Set ajax calls to buttons

--- public/js/teams-ajax.js
+++ public/js/teams-ajax.js
@@ -1,3 +1,34 @@
 $(document).ready(function() {
+  $('#edit-form').submit(function(e) {
+    e.preventDefault();
+    let teamUrl = $(this).attr('href');
+    let teamData = $(this).serialize();
 
+    $.ajax({
+      method: "PUT",
+      url: teamUrl,
+      data: teamData
+    }).done( data => {
+      console.log('Updated', data);
+      window.location = '/teams';
+    }).fail( err => {
+      console.log("Err:", err);
+    })
+  });
+
+  $('#delete-btn').click(function(e) {
+    e.preventDefault();
+    let teamUrl = $(this).attr('href');
+    console.log(teamUrl);
+
+    $.ajax({
+      method: "DELETE",
+      url: teamUrl
+    }).done( data => {
+      console.log('Bingo!', data);
+      window.location = '/teams';
+    }).fail( err => {
+      console.log("Err:", err)
+    });
+  });
 });
